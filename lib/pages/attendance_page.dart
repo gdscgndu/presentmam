b4456bc6e55c7a08a7d87c2fc8e578532a3a1a6f
sairish2001 <sairish2001@gmail.com>
2021-10-29 15:46:03 +0530

    unstructured display view of data added

--- lib/pages/attendance_page.dart
+++ lib/pages/attendance_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:csv/csv.dart';
 
 class AttendancePage extends StatefulWidget {
   const AttendancePage({Key? key}) : super(key: key);
@@ -13,19 +17,28 @@ class _AttendancePageState extends State<AttendancePage> {
   Widget uploadbtn() {
     return RaisedButton(
       child: Text('Upload'),
-      onPressed: () async {
+      onPressed: () async{
         var picked = await FilePicker.platform.pickFiles();
-
         if (picked != null) {
+          PlatformFile file = picked.files.first;
+          var pathoffile=file.path.toString();
+          final input = new File(pathoffile).openRead();
+          final fields = await input
+              .transform(utf8.decoder)
+              .transform(new CsvToListConverter())
+              .toList();
+          var data='';
+          for(int i=0;i<fields.length;i++){
+            data+=fields[i][0]+fields[i][1].toString()+"\n";
+          }
           setState(() {
-            filename = picked.files.first.name;
+            filename = data;
           });
-          // print(picked.files.first.name);
+          // print(picked.files.first.bytes);
         }
       },
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
